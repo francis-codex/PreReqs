3fd930d4431c3797ad3b8cd9e4808c0a2a54fae9
francis-codex <francislogo2219@gmail.com>
2024-10-09 02:48:29 +0100

    feat: updates

---
+++ airdrop/enroll.ts
@@ -0,0 +1,4 @@
+import { Connection, Keypair, PublicKey } from "@solana/web3.js"
+import { Program, Wallet, AnchorProvider } from "@coral-xyz/anchor"
+import { IDL, Turbin3Prereq } from "./programs/Turbin3_prereq";
+import wallet from "./Turbin3-wallet.json"
---
+++ programs/Turbin3_prereq.ts
@@ -0,0 +1,4 @@
+export type Turbin3Prereq = { "version": "0.1.0", "name": "Turbin3_prereq"}
+
+export const IDL: Turbin3Prereq = { "version": "0.1.0", "name": "Turbin3_prereq"}
+
